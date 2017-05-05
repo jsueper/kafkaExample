5539fff89b749d5731a2fa3ead81ee14c1b332f1
Shehryar Abbasi <shehryar.abbasi@gmail.com>
2017-05-04 21:45:05 -0600

    Update kakfaInstance.tf

--- kakfaInstance.tf
+++ kakfaInstance.tf
@@ -1,22 +1,17 @@
-data "aws_ami" "kafka" {
-  most_recent = true
 
-  filter {
-    name   = "name"
-    values = ["kafka-*"]
-  }
-
-  owners = ["self"]
+data "template_file" "user_data_script" {
+  template = "${file("${path.module}/user-data-script.sh")}"
 }
 
 resource "aws_instance" "kafka" {
-  ami           = "${data.aws_ami.kafka.id}"
+  ami           = "ami-4836a428"
   instance_type = "t2.micro"
   iam_instance_profile = "${aws_iam_instance_profile.kafka_instance_profile.name}"
   vpc_security_group_ids = ["sg-888dabf1"]
   subnet_id = "subnet-24f6877c"
   associate_public_ip_address = true
-  key_name = "dev2"
+  key_name = "dev"
+  user_data = "${data.template_file.user_data_script.rendered}"
   tags {
     Name = "kafka"
   }
