33c8154d72bb3f13dbc958a3e7f15acea3edb9ea
Shehryar Abbasi <shehryar.abbasi@gmail.com>
2017-05-04 23:15:04 -0600

    Update user-data-script.sh

--- user-data-script.sh
+++ user-data-script.sh
@@ -3,11 +3,16 @@ sudo yum update -y
 cd /tmp
 sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
 cd ~
-wget http://www-us.apache.org/dist/kafka/0.9.0.1/kafka_2.11-0.9.0.1.tgz
-tar -xvf kafka_2.11-0.9.0.1.tgz
-sudo mv kafka_2.11-0.9.0.1 /opt
+if [ ! -d /opt/kafka_2.11-0.9.0.1 ]
+then
+    wget http://www-us.apache.org/dist/kafka/0.9.0.1/kafka_2.11-0.9.0.1.tgz
+    tar -xvf kafka_2.11-0.9.0.1.tgz
+    sudo mv kafka_2.11-0.9.0.1 /opt
+    cd /opt/kafka_2.11-0.9.0.1
+    sudo sed -i 's/#port=9092/port=9092/' config/server.properties
+    sudo sed -i 's/#host.name=localhost/host.name=localhost/' config/server.properties
+fi
+(sudo bin/zookeeper-server-start.sh -daemon config/zookeeper.properties)
+(sudo bin/kafka-server-start.sh config/server.properties)
 cd /opt/kafka_2.11-0.9.0.1
-sudo bin/zookeeper-server-start.sh -daemon config/zookeeper.properties
-sudo bin/kafka-server-start.sh config/server.properties
-cd /opt/kafka_2.11-0.9.0.1
-sudo bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic shehryarsTopic
+(sudo bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic shehryarsTopic)
