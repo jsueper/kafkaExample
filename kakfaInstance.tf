d7922361b1ecf43ece97e508f3c723db23642570
Shehryar Abbasi <shehryar.abbasi@gmail.com>
2017-05-06 17:48:16 -0600

    Update kakfaInstance.tf

--- kakfaInstance.tf
+++ kakfaInstance.tf
@@ -16,17 +16,3 @@ resource "aws_instance" "kafka" {
     Name = "kafka"
   }
 }
-
-resource "aws_instance" "kafka2" {
-  ami           = "ami-4836a428"
-  instance_type = "m4.large"
-  iam_instance_profile = "${aws_iam_instance_profile.kafka_instance_profile.name}"
-  vpc_security_group_ids = ["sg-888dabf1"]
-  subnet_id = "subnet-24f6877c"
-  associate_public_ip_address = true
-  key_name = "dev"
-  user_data = "${data.template_file.user_data_script.rendered}"
-  tags {
-    Name = "kafka2"
-  }
-}
