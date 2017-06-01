ef81e524c38c2d08c78f6a7f41769684b0aa5f86
Shehryar Abbasi <shehryar.abbasi@gmail.com>
2017-06-01 13:22:31 -0500

    Update user-data-script.sh

--- user-data-script.sh
+++ user-data-script.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
-sudo yum update -y
-sudo yum install -y git gcc ruby-devel rubygems rake
-sudo gem install io-console serverspec
-echo "1\n2" | serverspec-init
-cd /home/ec2-user/
-git clone https://github.com/ShehryarAbbasi/kafkaExample.git
-cd kafkaExample/spec
-rake spec
 
+# update yum, install AWS SSM agent
+sudo yum update -y
 cd /tmp
 sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+
+# install and run Zookeeper + Kafka in the background, create a generic Kafka Topic
 cd ~
 kafkaVer="kafka_2.11-0.10.2.0"
 if [ ! -d /opt/$kafkaVer ]
@@ -23,8 +19,14 @@ then
     sudo sed -i "s|#advertised.listeners=PLAINTEXT:.*:9092|advertised.listeners=PLAINTEXT:\/\/$publicIP:9092|" config/server.properties
 fi
 (sudo bin/zookeeper-server-start.sh -daemon config/zookeeper.properties)
-(sudo bin/kafka-server-start.sh config/server.properties)
+(sudo bin/kafka-server-start.sh -daemon config/server.properties)
 cd /opt/$kafkaVer
 (sudo bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic testTopic)
 
+# pull down repo to run serverspec tests
 
+sudo yum install -y git gcc ruby-devel rubygems rake
+sudo gem install io-console serverspec && echo "1\n2" | serverspec-init
+cd /home/ec2-user/
+git clone https://github.com/ShehryarAbbasi/kafkaExample.git && cd kafkaExample
+rake spec
